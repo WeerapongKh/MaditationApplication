123c2e5b2d79f6a7292bd1bb16df2aa2ca30540e
Wiphada <wiphada.c@kkumail.com>
2022-03-05 22:56:00 +0700

    commit

---
+++ maditation/lib/page/detail.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//ข้อแนะนำในการนั่งสมาธิ
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: new Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        new Container(
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage("assets/images/detail.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+}
